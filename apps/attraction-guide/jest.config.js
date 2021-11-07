b2763888384e7d3a9171a7fa657c4de03d543cd8
Leo Lin <contemplation8213@gmail.com>
2021-11-07 18:04:36 +0800

    fix: 修正 application 錯誤的名稱

--- apps/attraction-guide/jest.config.js
+++ apps/attraction-guide/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'attration-guide',
+  displayName: 'attraction-guide',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/attration-guide',
+  coverageDirectory: '../../coverage/apps/attraction-guide',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
