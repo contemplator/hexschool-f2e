8d7f99c2235eb1509ab826b964f92338e0708560
Leo Lin <contemplation8213@gmail.com>
2021-11-11 00:59:10 +0800

    fix: 修正 build 錯誤

--- apps/hexschool-f2e/src/app/app.module.ts
+++ apps/hexschool-f2e/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
